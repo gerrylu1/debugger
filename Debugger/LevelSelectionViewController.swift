3c86065e0a17dcbafd90f8cc7cd1b8bc747338ad
gerrylow2019@gmail.com <gerrylu1@hotmail.com>
2020-06-23 15:59:56 +0800

    chore: Setup working directory

---
+++ Debugger/LevelSelectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LevelSelectionViewController.swift
+//  Debugger
+//
+//  Created by Gerry Low on 2020-06-23.
+//  Copyright © 2020 Gerry Low. All rights reserved.
+//
+
+import UIKit
+
+class LevelSelectionViewController: UITableViewController {
+    
+}
---
+++ Debugger/PlayerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerViewController.swift
+//  Debugger
+//
+//  Created by Gerry Low on 2020-06-23.
+//  Copyright © 2020 Gerry Low. All rights reserved.
+//
+
+import Foundation

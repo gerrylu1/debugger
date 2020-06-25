3567e01ceb1fe57b6017019abbeaa686d146504b
gerrylow2019@gmail.com <gerrylu1@hotmail.com>
2020-06-26 03:50:09 +0800

    feat: Change segue to about view to modal

---
+++ Debugger/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  Debugger
+//
+//  Created by Gerry Low on 2020-06-26.
+//  Copyright © 2020 Gerry Low. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func doneBarButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
